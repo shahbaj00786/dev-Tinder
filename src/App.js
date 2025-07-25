555bc219eb8194ad8ed961cd5ec4b32c98f3eb1b
shahbaj00786 <shahbajhussain231@gmail.com>
2025-07-25 09:40:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
